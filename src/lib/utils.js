9d8a9242035a97dc88f6e2ad9324e5d257a0551f
OPGT6 <onlineprogamingtournament@gmail.com>
2025-03-28 03:38:35 +0100

    Add UI components

---
+++ src/lib/utils.js
@@ -0,0 +1,14 @@
+[main 85d90da] Add configuration files
+ 4 files changed, 129 insertions(+)
+ create mode 100644 postcss.config.js
+ create mode 100644 src/index.css
+ create mode 100644 src/main.jsx
+ create mode 100644 tailwind.config.js
+Enumerating objects: 9, done.
+Counting objects: 100% (9/9), done.
+Delta compression using up to 12 threads
+Compressing objects: 100% (7/7), done.
+Writing objects: 100% (7/7), 1.56 KiB | 1.56 MiB/s, done.
+Total 7 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+To https://github.com/OPGT6/gaming-tournaments.git
+   49bfaa8..85d90da  main -> main
